31efba87cb49ba35aa5897e3a8d74ac2425d4006
liyongan <hzliyongan@corp.netease.com>
2017-11-28 15:08:11 +0800

    增加最简单的坑位匹配

---
+++ plugindemo/app/src/main/java/com/lya/plugindemo/PluginContainerActivity.java
@@ -0,0 +1,29 @@
+package com.lya.plugindemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PluginContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plugin_container);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- plugindemo/pluginengine/src/main/java/com/lya/pluginengine/PluginContainerManager.java
+++ plugindemo/pluginengine/src/main/java/com/lya/pluginengine/PluginContainerManager.java
@@ -4,13 +4,53 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 
+import java.util.HashMap;
+
 /**
  * Created by liyongan on 17/11/27.
  */
 
 public class PluginContainerManager {
 
+    private HashMap<String, ActivityState> mStates = new HashMap<>();
+
+    public PluginContainerManager() {
+        ActivityState activityState = new ActivityState();
+        activityState.container = "com.lya.plugindemo.PluginContainerActivity";
+        mStates.put(activityState.container, activityState);
+    }
+
+    class ActivityState {
+        String container;
+        String pluginName;
+        String activityName;
+    }
+
     public ComponentName loadPluginActivity(ActivityInfo activityInfo, Intent intent, String packageName, String activity) {
+        ActivityState target = null;
+        for (ActivityState activityState : mStates.values()) {
+            activityState.pluginName = packageName;
+            activityState.activityName = activity;
+            target = activityState;
+            break;
+        }
+        if (target == null) {
+            return null;
+        }
+        String containerName = target.container;
+        return new ComponentName(PluginEngine.getInstance().getContext().getPackageName(), containerName);
+    }
+
+    final ActivityState lookupByContainer(String container) {
+        if (container == null) {
+            return null;
+        }
+
+        HashMap<String, ActivityState> map = mStates;
+        ActivityState state = map.get(container);
+//        if (state != null && state.state != STATE_NONE) {
+//            return new ActivityState(state);
+//        }
         return null;
     }
 }
